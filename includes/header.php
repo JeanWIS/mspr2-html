a3dd570780a7bc0a44dddc34bcd4791eb35fe962
Jean <jdestoppani@gmail.com>
2020-02-04 15:00:31 +0100

    update: homepage

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <!—
+    GOogle font link —>
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo isset($title) ? $title : 'Page with no title' ; ?></title>
     <link rel="stylesheet" href="public/css/app.css">
@@ -12,12 +15,12 @@
 
 <header>
     <div class container>
-        <h1 class="VOCALLY"><a href="index.html"></h1>
+        <h1 class="VOCALLY"><a href="#"></h1>
     </div>
     <nav>
-        <a class="site-logo" href="/"><img src="public/images/logo.png" alt="Logo" ></a>
+        <a class="site-logo" href="/"> <img src="public/images/logo.png" alt="Logo" ></a>
         <ul id="navig">
-            <li id="image1"><a href="index.html">Accueil</a></li>
+            <li id="image1"><a href="#">Accueil</a></li>
             <li><a href="#">Article 1</a></li>
             <li><a href="#">Article 2</a></li>
             <li><a href="#" >Article 3</a></li>
--- index.php
+++ index.php
@@ -1,49 +1,80 @@
 <?php include_once 'includes/header.php'; ?>
 
-<section id="jumbotron">
-    <div>
-        <h1 id="testt">L'application qui vous permet de comuniquer avec n'importe qui</h1>
-        <img src="includes/images/image1.jpg">
-        <button>EN savoir plus</button>
+<div class="grid-2" id="jumbotron"> <!-- parent div (ou autre) de 3 colonnes call you call hola -> jumbotron -->
+    <div class="text1">
+        <h1>
+            L'application qui vous permet de comuniquer avec n'importe qui
+        </h1>
+        <h5>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+        </h5>
+        <button class="btn--primary">en savoir plus</button>
     </div>
-</section>
-
-<section class="section">
-    <p>Youtube video</p>
-    <!— iframe —>
-</section>
-
-<section id="our_community" class="section">
-    <p>REjoiniez une communité de plus d'un million de personnes </p>
-    <img src="includes/images/flag1.jpg">
-    <img src="includes/images/flag2.jpg">
-    <img src="includes/images/flag3.jpg">
-    <img src="includes/images/flag4.jpg">
-</section>
-
-<section class="section">
-    <div>
-        <p>"Utiliser Vocally m'a permis de communiquer avec ma famille éloignée du Costa-Rica alors que je suis une brel
-            en
-            Espagnol"</p>
+    <div style="width: 50%">
+        <img src="public/images/discussion.jpg">
     </div>
+</div>
 
-    <div>
-        <h2>Des conversations de qualité </h2>
-        <div class="col">
-            <h3>C'est la fin qu'une conversation sur google traduction qui est incompréhnesilbe </h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore
-                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit
-                anim id est laborum</p>
+<div class="video" style="margin: 40px">
+    <img src="public/images/video.png">
+</div>
+
+<div class="companies" class="section">
+    <h2> Rejoiniez une communité de plus d'un
+        million d'entreprise qui nous font confiance
+    </h2>
+    <section class="grid-4 has-gutter" style="justify-content: center">
+        <div>
+            <img src="public/images/cp.png">
         </div>
-    </div>
+        <div>
+            <img src="public/images/cp1.png">
+        </div>
+        <div>
+            <img src="public/images/cp2.png">
+        </div>
+        <div>
+            <img src="public/images/cp3.png">
+        </div>
+        <div>
+            <img src="public/images/cp4.png">
+        </div>
+        <div>
+            <img src="public/images/cp5.png">
+        </div>
+        <div>
+            <img src="public/images/cp6.png">
+        </div>
+        <div>
+            <img src="public/images/cp7.png">
+        </div>
+    </section>
 
-</section>
+</div>
+
+
+<section class="section"></section>
+
+
+<div class="grid-2" style="justify-items: center; font-size: 20px; "> <!-- parent div (ou autre) de 3 colonnes -->
+    <div style="width: 80%">
+        <h2><strong>Des conversations de qualité </strong> </h2>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+        deserunt mollit anim id est laborum
+    </div>
+    <img src="public/images/pict1.png" style="width: 50%" >
+</div>
+
+
+
+
 <?php include_once 'includes/footer.php'; ?>
 
 
